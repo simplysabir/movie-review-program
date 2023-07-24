2631ffd3ad0dc7f60fe2809baf3bef4e22cfabe4
simplysabir <simplysabir@gmail.com>
2023-07-24 15:23:06 +0530

    update

--- src/state.rs
+++ src/state.rs
@@ -1,19 +1,55 @@
 use borsh::{BorshSerialize, BorshDeserialize};
 use solana_program::{
+    // We're bringing in Pubkey
+    pubkey::Pubkey,
     program_pack::{IsInitialized, Sealed},
 };
 
 #[derive(BorshSerialize, BorshDeserialize)]
 pub struct MovieAccountState {
+    // Two new fields added - discriminator and reviewer
+    pub discriminator: String,
     pub is_initialized: bool,
+    pub reviewer: Pubkey,
     pub rating: u8,
     pub title: String,
     pub description: String,
 }
 
+// New struct for recording how many comments total
+#[derive(BorshSerialize, BorshDeserialize)]
+pub struct MovieCommentCounter {
+    pub discriminator: String,
+    pub is_initialized: bool,
+    pub counter: u64,
+}
+
+// New struct for storing individual comments
+#[derive(BorshSerialize, BorshDeserialize)]
+pub struct MovieComment {
+    pub discriminator: String,
+    pub is_initialized: bool,
+    pub review: Pubkey,
+    pub commenter: Pubkey,
+    pub comment: String,
+    pub count: u64,
+}
+
 impl Sealed for MovieAccountState {}
 
 impl IsInitialized for MovieAccountState {
+    fn is_initialized(&self) -> bool {
+        self.is_initialized
+    }
+}
+
+impl IsInitialized for MovieCommentCounter {
+    fn is_initialized(&self) -> bool {
+        self.is_initialized
+    }
+}
+
+impl IsInitialized for MovieComment {
     fn is_initialized(&self) -> bool {
         self.is_initialized
     }
